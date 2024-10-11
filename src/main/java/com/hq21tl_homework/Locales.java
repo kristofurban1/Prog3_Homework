350dba031b1df8083bcbf6105d0506b86a1d1da9
Urban Kristof <kristofurban1@gmail.com>
2024-10-11 17:58:21 +0200

    Localization. Added event on language change

--- src/main/java/com/hq21tl_homework/Locales.java
+++ src/main/java/com/hq21tl_homework/Locales.java
@@ -1,5 +1,6 @@
 package com.hq21tl_homework;
 
+import java.beans.EventHandler;
 import java.io.FileNotFoundException;
 
 import org.w3c.dom.Document;
@@ -10,6 +11,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -20,6 +22,29 @@ import com.hq21tl_homework.Utils.KeyValuePair;
 //EagerInitializedSingleton
 @SuppressWarnings("java:S6548") // Singleton is intentionally not exposing an instance.
 public class Locales { 
+
+    public static interface LocalizationChangeListener {
+        void localizationChanged();
+    }
+    public static class LocalizationChangeEventHandler {
+        private final List<LocalizationChangeListener> listeners = new ArrayList<>();
+
+        // Method to add listeners
+        public void addValueChangeListener(LocalizationChangeListener listener) {
+            listeners.add(listener);
+        }
+
+        // Method to remove listeners
+        public void removeValueChangeListener(LocalizationChangeListener listener) {
+            listeners.remove(listener);
+        }
+        // Fire event to all registered listeners
+        public void fireLocalizationChanged() {
+            for (LocalizationChangeListener listener : listeners) {
+                listener.localizationChanged();
+            }
+        }
+    }
     
     public static class LocaleFormatException extends Exception{
         public LocaleFormatException(String msg){
@@ -32,9 +57,10 @@ public class Locales {
         }
     }
 
+    public static final LocalizationChangeEventHandler eventHandler = new LocalizationChangeEventHandler();
+
     // Singleton field
     private static final Locales instance  = new Locales();
-
 
     private static final String LOCALES_AVAILABLE = "locales.xml";
     private static final String LOCALES_AVAILABLE_ROOT = "locales";
@@ -42,7 +68,6 @@ public class Locales {
     private static final String LOCALES_AVAILABLE_ENTRY_ATTR_NAME = "name";
     private static final String LOCALES_AVAILABLE_ENTRY_ATTR_DEFAULT = "default";
 
-    
     private static final String LOCALES_FOLDER = "locales/";
     private static final String LOCALES_EXTENSION = ".xml";
     private static final String LOCALES_ROOT = "strings";
@@ -64,6 +89,7 @@ public class Locales {
     private static final String ERRMSG_FORMAT_LOCALE_ENTRY_DUPLICATE = "File: " +LOCALES_FOLDER+"%s.xml -- Contains duplicate entries with ID: %s";
 
     private static final String ERRMSG_UNAVAILABLE_NO_LOCALES_LOADED = "File: " +LOCALES_FOLDER+LOCALES_AVAILABLE + " -- No locale entries in file.";
+    private static final String ERRMSG_UNAVAILABLE_NO_LOCALE_FOUND = "File: " +LOCALES_FOLDER+LOCALES_AVAILABLE + " -- No locale entry \"%s\" in file.";
 
     private static final String LOCALE_MISSING_STRING = "-- MISSING STRING --";
 
@@ -142,7 +168,7 @@ public class Locales {
                 throw new LocaleUnavailableException(ERRMSG_UNAVAILABLE_NO_LOCALES_LOADED);
             }
 
-            // Convert the arraylist into a fixed lenght string
+            // Convert the arraylist into a fixed lenght string array.
             @SuppressWarnings("unchecked")  // Suppress the unchecked cast warning
             KeyValuePair<String, String>[] _availableLocales = availableLocalesBuilder.toArray(KeyValuePair[]::new); //NOSONAR // Var starting with '_', could be named ...Temp
             this.availableLocales = _availableLocales;
@@ -157,17 +183,33 @@ public class Locales {
         switchInstanceLanguage(availableLocales[0].getKey());
     }
 
-    private void switchInstanceLanguage(String lang){
-        // If we are already at the desired language, don't reaload locale.
-        if (lang.equals(selectedLocale))
-            return;
-        
-        // Set selected
-        selectedLocale = lang;
+    private void verifyLanguageAvailable(String lang) throws LocaleUnavailableException{
+        boolean langAvailable = false;
+        for ( KeyValuePair<String,String> locale : availableLocales){
+            if (lang.equals(locale.getKey())) {
+                langAvailable = true;
+                break;
+            }
+        }
+        if (!langAvailable)
+            throw new LocaleUnavailableException(String.format(ERRMSG_UNAVAILABLE_NO_LOCALE_FOUND, lang));
+    }
 
-        // Clear previous locale data.
-        localeStringsMap.clear();
+    private void switchInstanceLanguage(String lang){
         try {
+            // If we are already at the desired language, don't reaload locale.
+            if (lang.equals(selectedLocale))
+                return;
+            
+            verifyLanguageAvailable(lang);
+            
+            // Set selected
+            selectedLocale = lang;
+
+            // Clear previous locale data.
+            localeStringsMap.clear();
+
+            // Reading locale file
             Document doc;
             String filename = LOCALES_FOLDER + lang + LOCALES_EXTENSION;
             try ( 
@@ -176,33 +218,40 @@ public class Locales {
             }
             if (doc == null) throw new FileNotFoundException(String.format(ERRMSG_FILE_NOT_FOUND, filename));
             
-
+            // Getting root of file, must be names "strings"
             Element root = doc.getDocumentElement();
             if (!root.getNodeName().equals(LOCALES_ROOT))
                 throw new LocaleFormatException(String.format(ERRMSG_FORMAT_LOCALE_ROOT, lang));
             
+            // Root must have a "locale" attribute with the value of locale
             if (!root.hasAttribute(LOCALES_ATTR_LOCALE))
                 throw new LocaleFormatException(String.format(ERRMSG_FORMAT_LOCALE_ROOT_LOCALE, lang));
             
+            // This locale attribute's value must equal the one given in file name. 
             if (!root.getAttribute(LOCALES_ATTR_LOCALE).equals(lang))
                 throw new LocaleFormatException(String.format(ERRMSG_FORMAT_LOCALE_ROOT_LOCALE_VALUE, lang, root.getAttribute(LOCALES_ATTR_LOCALE)));
             
+            // Iterate over the children of root.
             Node entryNode = root.getFirstChild();
             while(entryNode != null){
+                // If the node is not type Element then ignore it.
                 if (!(entryNode instanceof Element)) {
                     entryNode = entryNode.getNextSibling();
                     continue;
                 }
                 Element entry = (Element)entryNode;
 
+                // Entry must be named "string"
                 if (!entry.getNodeName().equals(LOCALES_ENTRY))
                     throw new LocaleFormatException(String.format(ERRMSG_FORMAT_LOCALE_ENTRY, lang));
                 
+                // Entry must have attribute "ID". This is the key the string's value will be accessed by.
                 if (!entry.hasAttribute(LOCALES_ENTRY_ATTR_ID))
                     throw new LocaleFormatException(String.format(ERRMSG_FORMAT_LOCALE_ENTRY_ID, lang));
                     
                 String id = entry.getAttribute(LOCALES_ENTRY_ATTR_ID);
-                    
+                
+                // Filter out duplicated IDs
                 if (localeStringsMap.containsKey(id))
                     throw new LocaleFormatException(String.format(ERRMSG_FORMAT_LOCALE_ENTRY_DUPLICATE, lang, id));
 
@@ -212,14 +261,19 @@ public class Locales {
             }
             
             
-        } catch (LocaleFormatException | IOException | SAXException | ParserConfigurationException e) {
+        } catch (LocaleUnavailableException | LocaleFormatException | IOException | SAXException | ParserConfigurationException e) {
             // TODO: Dispaly custom error window.
             e.printStackTrace();
         }
     }
 
+    /**
+     * Switching localizaation langugage. Fires LocalizationChanged Event
+     * @param lang The language that the localization should switch to.
+     */
     public static void switchLanguage(String lang){
         instance.switchInstanceLanguage(lang);
+        eventHandler.fireLocalizationChanged();
     }
     public static String getString(String key){
         return instance.localeStringsMap.getOrDefault(key, LOCALE_MISSING_STRING);
